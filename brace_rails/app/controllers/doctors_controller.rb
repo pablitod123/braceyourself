e4a580b3805f6d80a5eed373bb4b0b36c32e69ee
Paul Styslinger <pablitod123@gmail.com>
2014-07-22 13:49:55 -0400

    added drilldown chart to doctor rails page

--- brace_rails/app/controllers/doctors_controller.rb
+++ brace_rails/app/controllers/doctors_controller.rb
@@ -1,10 +1,12 @@
 class DoctorsController < ApplicationController
+
 	def show
 	# id = Doctor.find_by(user_id: current_user.id).id
 	@allpatients = (Doctor.find(params[:id]).patients)
 	end
 
 	def showpatient
+	@thispatient = User.find(Patient.find(params[:id]).user_id)
 	end 
 
 end
