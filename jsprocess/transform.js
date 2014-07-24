7b04ebb0d62d8be6c01cfd206bfff98fc9a783fb
Marcus Russi <marcusrussi@gmail.com>
2014-07-24 17:41:12 -0400

    Removed node_modules folder, perfected data pipeline, cleaned up transform.js

--- jsprocess/transform.js
+++ jsprocess/transform.js
@@ -37,12 +37,10 @@ fs.readFile('day.csv', function(err, file) {
 
     function nestedFunctionalMap(struct, fList) {
       function g(struct) {
-        if (_.isEmpty(this.fList)) {
+        if (_.isEmpty(this.fList))
           return struct;
-        }
 
         var nextFunc = _.first(this.fList);
-
         var nextThis = {fList: _.rest(this.fList)};
 
         return nextFunc(struct, g.bind(nextThis));
@@ -51,102 +49,73 @@ fs.readFile('day.csv', function(err, file) {
       return g.apply({fList: fList}, [struct]);
     }
 
-    var f = function(struct, q) { var obj1 = {}; var obj2 = {};
-                              obj1[struct[0]] = struct[0]; obj2[struct[1]] = struct[1];
-                              return [q(obj1), q(obj2) , 3] };
+    var structureTransforms = {    
+      setupTopLevelData: function(original, q) {
+        return {
+          name: 'data',
+          children: q(original)
+        };
+      },
 
-    var g = function(struct, q) { var k = _.keys(struct); struct[k[0]] = struct[k[0]] * 100; return q(struct); };
-
-    console.log(nestedFunctionalMap([1, 2, 3], [f, g]));
-
-
-
-
-
-
-
-
-
-
-
-    // Given 'struct' and an array of functions 'fList', recursively
-    // applies the first function in fList to struct, doing a
-    // deep recursive map on 'struct' if 'struct' is an array
-    // or object.  Makes modifications shallow-to-deep.  Returns
-    // at deepest invocation of last function.
-    function nestedMap(struct, fList) {
-      if (fList.length === 0) {
-        return struct;
-      }
-
-      var newStruct = (fList[0])(struct);
-
-      if (Array.isArray(newStruct) || typeof newStruct === 'object') {
-        return _.map(newStruct, function(item) {
-          return nestedMap(item, _.rest(fList));
+      mapMonths: function(original, q) {
+        return _.map(original, function(month) {
+          var monthNum = _.keys(month)[0];
+          return {
+            name: 'Month #' + monthNum,
+            children: q(month[monthNum])
+          };
         });
-      } else {
-        return nestedMap(newStruct, _.rest(fList));
+      },
+
+      transformWeekObjects: function(monthObj, q) {
+        var keys = _.keys(monthObj);
+        var weeks = [];
+        _.each(keys, function(key) {
+          weeks.push(monthObj[key]);
+        });
+
+        return q(weeks);
+      },
+
+      createWeekStructure: function(weeksArray, q) {
+        return _.map(weeksArray, function(week) {
+          return {
+            name: 'Week ' + week[0].week,
+            children: q(week)
+          };
+        });
+      },
+
+      aliasDayName: function(week, q) {
+        week.name = week.dayName;
+        return q(week);
+      },
+
+      aliasPosition: function(week, q) {
+        week.size = week.position;
+        return q(week);
       }
-    }
+    };
 
-    // Example to be transformed
-    // var q = [[1, 2, 3],
-    //          [2, 3, 4],
-    //          [3, 4, 5]];
-
-//      var q = [
-//                { '6':
-//                    { '29': [Object], '30': [Object], '31': [Object] }
-//                },
-//                { '7':
-//                    { '31': [Object], '32': [Object], '33': [Object] }
-//                }
-//              ];
-
-    // Goal of transformation
-    // var r = {
-    //           {
-    //           "name": 'June',
-    //           "children": [
-    //               { '29': [Object], '30': [Object], '31': [Object] }
-    //             ]
-    //           },
-    //           {
-    //           "name": 'July',
-    //           "children": [
-    //               { '31': [Object], '32': [Object], '33': [Object] }
-    //             ]
-    //           }
-    //         };
-
-
-    // Array of functions to perform the transformation from
-    // array 'q' to array 'r'
-    var funcs = [function(arr) { return _.object([0,1,2],arr); }];
-                 // function(num) { return num * 100},
-                 // function(num) { return _.range(0, num + 100, 100); }];
-
-    // Log for debugging
+    var pipeline = [ structureTransforms.setupTopLevelData,
+                     structureTransforms.mapMonths,
+                     structureTransforms.transformWeekObjects,
+                     structureTransforms.createWeekStructure,
+                     structureTransforms.aliasDayName,
+                     structureTransforms.aliasPosition ];
 
     // Create an Underscore chain from 'parsed' and apply
     // the chain of transformations, pulling it out of the
     // chain through .value()
-    var result = _.chain(parsed)
+    var organized = _.chain(parsed)
       .rest()
       .map(addNewProps)
       .groupBy('month')
       .map(groupByWeekInMonth)
       .value();
 
-    console.log(result);
+    var structured = nestedFunctionalMap(organized, pipeline);
 
-    // fs.writeFile("transformedtest.json", JSON.stringify(result), function(err) {
-    //   console.log(err);
-    // })
-
-    // Uncomment for debug.
-    //console.log(result);
-    //console.log(result[0]['6']);
+    console.log(structured.toString());
   });
 });
